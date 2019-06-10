c6a1fb564f97203732b871e5bd6c2ab90db181a0
Tommy Tran <tranttommy95@gmail.com>
2019-06-10 10:39:09 -0700

    installed redux + redux-react and hooked to app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 import App from './components/App';
+import Store from './Store';
 
 render(
-  <App />,
+  <Provider store={Store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
