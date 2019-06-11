f562c374cd4b0401f7aa01f48cfcd809b43e5105
Tommy Tran <tranttommy95@gmail.com>
2019-06-11 15:59:58 -0700

    added copy-webpack-plugin + redirect

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const DotEnv = require('dotenv-webpack');
+const CopyPlugin = require('copy-webpack-plugin');
 
 // eslint-disable-next-line
 module.exports = {
@@ -16,7 +17,10 @@ module.exports = {
   plugins: [
     new HtmlPlugin({ template: './src/index.html' }),
     new CleanWebpackPlugin(),
-    new DotEnv({ systemvars: true })
+    new DotEnv({ systemvars: true }),
+    new CopyPlugin([
+      { from: 'public' }
+    ])
   ],
   module: {
     rules: [
